47acf56e9019273155d5b4d4c160e1bc2d84d652
Johnny Chang <66932108+hiimdoublej-swag@users.noreply.github.com>
2023-10-31 11:37:55 +0800

    Update

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
-'''
- ___ _ _ _ ___ ___
-|_ -| | | | .'| . |
-|___|_____|__,|_  |
-              |___|
-'''
-import gevent.monkey; gevent.monkey.patch_all()
+import gevent.monkey
+
+gevent.monkey.patch_all()
+
 import flask
 
 
 app = flask.Flask(__name__)
-app.register_blueprint(flask.Blueprint('swag', __name__))
 
 @app.route('/helloworld', methods={'GET'}, endpoint='helloworld')
 def helloworld():
